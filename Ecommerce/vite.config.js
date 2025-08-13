e4773b067d1bc9b39a325367480adaf21d9ae934
jeevant010 <jeevantmudgil10@gmail.com>
2025-08-13 17:05:07 +0530

    looking for clearity

--- Ecommerce/vite.config.js
+++ Ecommerce/vite.config.js
@@ -2,20 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react()
   ],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true
-  },
-  server: {
-    port: 5173,
-    open: true
-  }
 })
