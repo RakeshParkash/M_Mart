b623960dcaf25879f37732cd2b529518f357b4b6
jeevant010 <jeevantmudgil10@gmail.com>
2025-08-13 16:27:03 +0530

    func check

--- Ecommerce/vite.config.js
+++ Ecommerce/vite.config.js
@@ -2,11 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react()
   ],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  },
+  server: {
+    port: 5173,
+    open: true
+  }
 })
