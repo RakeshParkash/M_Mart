55d8ac156732fd37c3469b2e595fd50992cb2e68
jeevant010 <jeevantmudgil10@gmail.com>
2026-06-02 02:06:57 +0530

    refactor: enhance history schema with additional fields and type definitions

--- Backend/models/History.js
+++ Backend/models/History.js
@@ -1,15 +1,22 @@
 const mongoose = require('mongoose');
 
 const historySchema = new mongoose.Schema({
-  type: { type: String, required: true }, // e.g., 'user-deletion'
+  type: { type: String, required: true }, // e.g., 'user-deletion', 'product-update', etc.
+  entityType: { type: String, required: false }, // 'User', 'Product', 'Receipt', 'CustomList', 'Purchase', 'Due'
+  action: { type: String, required: false }, // 'Create', 'Update', 'Delete'
   performedBy: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Admin',
     required: false, // In case admin gets deleted
   },
+  targetUser: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: false, // if the action is tied to a specific user
+  },
   data: {
-    type: Object,
-    required: true, // Store deleted user data
+    type: mongoose.Schema.Types.Mixed,
+    required: true, // Store the data that was changed
   },
   timestamp: {
     type: Date,
